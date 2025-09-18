6c7eca3553b9d9ed03ba45f46b05f1a907535c83
MIbrahimcheema <iambestibrahim92@gmail.com>
2025-09-18 04:43:17 -0700

    initial commit

---
+++ lesson 24/arrays.py
@@ -0,0 +1,7 @@
+import array as w
+wsp = w.array("i",[23,34,34,34,34,53,23,6465,757])
+print(wsp)
+print("The number of count of 34 is:",wsp.count(34))
+print("The reverse of this array is:")
+wsp.reverse()
+print(wsp)
---
+++ lesson 24/setintersestion.py
@@ -0,0 +1,6 @@
+set1 = {"green","blue","red"}
+print(set1)
+set2 = {"green","blue","blue","red"}
+print(set2)
+print("Intersections of 2 sets is:")
+print(set1.intersection(set2))
