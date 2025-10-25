cc5b24a40969c36e23b4fa7234bc614ff821ab91
MIbrahimcheema <iambestibrahim92@gmail.com>
2025-10-25 00:24:14 -0700

    initial commit

--- lesson 26/guessthenum.py
+++ lesson 26/guessthenum.py
@@ -1,11 +1,11 @@
 import random
 import time
-number = random.randint(1,100)
+number = random.randint(1,50)
 def intro():
     print("May i ask your name?")
     global name
     name = input()
-    print(name,"we will play a small game in it i will think between a number 1 to 100")
+    print(name,"we will play a small game in it i will think between a number 1 to 50")
     if number%2 == 0:
         x = "even"
     else:
@@ -15,14 +15,14 @@ def intro():
     print("go ahead and guess the number!")
 def pick():
     guesstaken = 0
-    while guesstaken<10:
+    while guesstaken<8:
         time.sleep(1)
         enter = input("GO guess it:")
         try:
             guess = int(enter)
-            if guess <= 100 and guess >= 1:
+            if guess <= 50 and guess >= 1:
                 guesstaken += 1
-                if guesstaken < 10:
+                if guesstaken < 8:
                     if guess<number:
                         print ("Your guess was low")
                     if guess>number:
@@ -32,17 +32,17 @@ def pick():
                         print("Try again")
                     if guess == number:
                         break
-            if guess > 100 or guess < 1:
+            if guess > 50 or guess < 1:
                 print("Your number is not in the range")
         except:
-            print("I dont think that the input you have give to me isa number")
+            print("I dont think that the input you have give to me is a number")
     if guess == number:
         guesstaken = str(guesstaken)
         print("Good job",name,"you guessed my number in",guesstaken,"guess")
     if guess != number:
-        print("Nopr the number i was thinking was",str(number))
+        print("Nope the number i was thinking was",str(number))
 playagain = "yes"
-while playagain == "yes" or playagain == "yea":
+while playagain == "yes" or playagain == "yea" or playagain == "yeah" or playagain == "sure" or playagain == "yup" or playagain == "Ofcourse":
     intro()
     pick()
     print ("Do you want to play again?")
